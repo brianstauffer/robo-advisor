ea9620ea5387483bc151c60a5767e4fc1c8f8885
brianstauffer <brian.m.stauffer@gmail.com>
2021-06-10 20:40:46 -0400

    Set up env

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,1 +1,27 @@
 print("To do: make a web request for some stock market data")
+
+
+# https://www.alphavantage.co/documentation/#dailyadj
+
+# setup
+import os
+from dotenv import load_dotenv
+import requests
+
+
+
+# read env variables
+load_dotenv() # go get env vars from the .env file
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+# make a request
+symbol = "MSFT" # ask for user input
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+
+print("TEST TEST")
+
+
+response = requests.get(request_url)
+print(type(response))
+print(response.text)
